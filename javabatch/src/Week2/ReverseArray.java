9ff7a8819189e6df97a9d1a2a71b7629e04d9fda
rishika1998 <rishikapriya.1306@gmail.com>
2023-06-07 13:19:29 -0500

    week 2 day 2 question 2

---
+++ javabatch/src/Week2/ReverseArray.java
@@ -0,0 +1,10 @@
+package Week2;
+
+public class ReverseArray {
+    public static void rev() {
+        int arr[] = {4, 6, 3, 2, 9};
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + "\t");
+        }
+    }
+}
