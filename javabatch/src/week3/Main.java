93488b8a49f2aab05cee95cbe0ca7b98b15f1336
rishika1998 <rishikapriya.1306@gmail.com>
2023-06-13 15:22:57 -0500

    day 11 question 1

---
+++ javabatch/src/week3/Main.java
@@ -0,0 +1,13 @@
+package week3;
+import week3.StringtoInteger;
+
+public class Main {
+    public static void main(String args[]){
+        //day 11 question 1
+        System.out.println("**** day 11 ****");
+        StringtoInteger sti=new StringtoInteger();
+        sti.test();
+        //day 11 question 2
+
+    }
+}
---
+++ javabatch/src/week3/StringtoInteger.java
@@ -0,0 +1,22 @@
+package week3;
+
+import java.util.Scanner;
+
+public class StringtoInteger {
+    public void test() {
+        try {
+            System.out.print("Enter a number :");
+            Scanner sc= new Scanner(System.in);
+            String str= sc.nextLine();
+
+            int i = Integer.parseInt(str);
+            System.out.print("String converted to number is :"+i);
+
+
+        } catch (NumberFormatException e) {
+
+            System.out.println("The number format exception");
+
+        }
+}
+}
