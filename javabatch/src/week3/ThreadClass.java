b6ef9f6d012e6a4890778b135f270196c804679e
rishika1998 <rishikapriya.1306@gmail.com>
2023-06-14 15:39:50 -0500

    day 12

--- javabatch/src/week3/ThreadClass.java
+++ javabatch/src/week3/ThreadClass.java
@@ -5,6 +5,7 @@ public class ThreadClass extends Thread{
     @Override
     public void start() {
         for (int i = 1; i <= 10; i++) {
+           // System.out.print("");
             System.out.println(i);
         }
     }
